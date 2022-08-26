f308ec9f421ff0a3b3f3ad866d194819d274474b
Bernard Niset <bernard.niset@gmail.com>
2022-08-26 17:46:21 +0200

    feat: implement fastify and support cli options

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,113 @@
-import http from "node:http";
+import commandLineArgs from "command-line-args";
+import commandLineUsage from "command-line-usage";
+import "dotenv/config";
+import { IncomingMessage, Server, ServerResponse } from "http";
+import { fastify, FastifyInstance } from "fastify";
 import { processRequest } from "./engine";
 import { loadConfiguration } from "./mapping";
 
-const start = async () => {
-  const configuration = await loadConfiguration();
+const environment = process.env.NODE_ENV ?? "production";
 
-  const server = http.createServer(async (req, res) => {
-    try {
-      await processRequest(configuration.mappings, req, res);
-    } catch (error) {
-      console.error("Error while processing request", error);
-    }
-  });
-  server.on("clientError", (err, socket) => {
-    socket.end("HTTP/1.1 400 Bad Request\r\n\r\n");
-  });
-  server.listen(4000);
-  console.log("Server running at http://localhost:4000/");
+type Options = {
+  help?: boolean;
+  port?: number;
+  host?: string;
+  logger?: string;
 };
 
-start().catch(console.error);
+const optionDefinitions = [
+  {
+    name: "help",
+    description: "Display this usage guide.",
+    alias: "h",
+    type: Boolean,
+  },
+  {
+    name: "port",
+    alias: "p",
+    description: "Specify the port to listen on.",
+    type: Number,
+  },
+  {
+    name: "host",
+    alias: "H",
+    description: "Specify the host to listen on.",
+    type: String,
+  },
+  {
+    name: "logger",
+    alias: "l",
+    description: "Specify the logger to use.",
+    type: String,
+  },
+];
+
+const optionUsage = [
+  {
+    header: "Dream Mock Server",
+    content: "Dream Mock Server",
+  },
+  {
+    header: "Options",
+    optionList: optionDefinitions,
+  },
+];
+
+let options: Options | undefined;
+try {
+  options = commandLineArgs(optionDefinitions) as Options;
+} catch (error) {
+  // ignore
+  console.log("Unrecognised option");
+}
+
+if (options === undefined || options["help"]) {
+  const usage = commandLineUsage(optionUsage);
+  console.log(usage);
+} else {
+  const logger =
+    options?.logger !== undefined
+      ? {
+          level: options.logger,
+          transport:
+            environment === "development"
+              ? {
+                  target: "pino-pretty",
+                  options: {
+                    translateTime: "HH:MM:ss Z",
+                    ignore: "pid,hostname",
+                  },
+                }
+              : undefined,
+        }
+      : false;
+  const start = async () => {
+    const configuration = await loadConfiguration();
+    const server: FastifyInstance<Server, IncomingMessage, ServerResponse> =
+      fastify({
+        logger,
+      });
+    server.get("/__admin", async (request, reply) => {
+      // await processRequest(configuration.mappings, request, reply);
+      reply.send("Admin routes");
+    });
+    server.all("/*", async (request, reply) => {
+      // await processRequest(configuration.mappings, request, reply);
+      // reply.send("All routes");
+      reply.hijack();
+      await processRequest(configuration.mappings, request.raw, reply.raw);
+    });
+    try {
+      const host = options?.host !== undefined ? { host: options.host } : {};
+      await server
+        .listen({ port: options?.port ?? 4000, ...host })
+        .then(
+          (address) => !logger && console.log(`server listening on ${address}`),
+        );
+    } catch (err) {
+      server.log.error(err);
+      process.exit(1);
+    }
+  };
+  start().catch(console.error);
+}
